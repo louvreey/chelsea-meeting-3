874553eaa064ab76c681fa82e50c3a2bf99e626c
Wincent Bastian <wincentbastian3@gmail.com>
2023-11-29 19:03:43 +0700

    fix meeting 3

--- Virtual_Painter/VirtualPainter.py
+++ Virtual_Painter/VirtualPainter.py
@@ -4,32 +4,32 @@ import time
 import os
 import HandTrackingModule as htm
 
-detector = htm.handDetector(detectionConfidence = 0.85)
+detector = htm.handDetector(detectionConfidence=0.85)
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-myListDirectory = os.listdir("C:\Meeting_3_MediaPipe\Virtual_Painter\header")
-print(myListDirectory)
+myListDirectory = os.listdir(
+    "/Users/wincentbastian/Documents/timedoor/ai-dev-18c/chelsea-meeting-3/Virtual_Painter/header")
 overlayList = []
 
+print(myListDirectory)
+
 for imPath in myListDirectory:
-    image = cv2.imread(f"C:\Meeting_3_MediaPipe\Virtual_Painter\header\{imPath}") 
-    overlayList.append(image) 
-    print(imPath)
-
-print(overlayList)
-
-
+    image = cv2.imread(
+        f"/Users/wincentbastian/Documents/timedoor/ai-dev-18c/chelsea-meeting-3/Virtual_Painter/header/{imPath}")
+    overlayList.append(image)
 
 header = overlayList[0]
 
 while True:
     res, frame = cap.read()
-    frame[0 : 125, 0 : 1280] = header
+    frame[0: 125, 0: 1280] = header
     frame = cv2.flip(frame, 1)
+    print(frame)
     frame = detector.findHands(frame)
+    lmList = detector.findPosition(frame, draw=True)
     cv2.imshow("Frame", frame)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
